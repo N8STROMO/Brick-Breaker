38a0e1d697faba674b8d53089b64db06b2a481d8
NO8STROMO <johntpaul93@gmail.com>
2017-01-23 12:56:45 -0500

    Created Instructions; Add StartGame script to move to level one under any key press

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour {
+
+    /// <summary>
+    /// After instructions are displayed press any key to start the game
+    /// </summary>
+	void Update () {
+
+        bool keyPress = Input.anyKey;
+
+        if(keyPress)
+        {
+            SceneManager.LoadScene("Level One");
+        }
+
+
+    }
+}
