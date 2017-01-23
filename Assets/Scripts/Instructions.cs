2673c10de2b390d6d18e95cd605e6d632c515828
NO8STROMO <johntpaul93@gmail.com>
2017-01-23 15:00:04 -0500

    Revert "Changed GameStart script to Instruction script; same code"
    This reverts commit 44616cd6312d436b83ea4a4a797a81010f3684e6.

--- Assets/Scripts/Instructions.cs
+++ Assets/Scripts/Instructions.cs
@@ -1,21 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Instructions : MonoBehaviour {
 
-    /// <summary>
-    /// After instructions are displayed press any key to start the game
-    /// </summary>
-    void Update()
-    {
-
-        bool keyPress = Input.anyKey;
-
-        if (keyPress)
-        {
-            SceneManager.LoadScene("Level One");
-        }
-    }
+	void Start () {
+		
+	}
 }
